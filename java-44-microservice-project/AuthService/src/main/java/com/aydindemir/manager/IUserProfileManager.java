d68f13ff62baabc81e3533a6bb68c217e468844c
Aydın Demir <46259600+aydindemir1@users.noreply.github.com>
2026-09-24 07:02:51 +0300

    feat(java-44): add service-name based load balancing

--- java-44-microservice-project/AuthService/src/main/java/com/aydindemir/manager/IUserProfileManager.java
+++ java-44-microservice-project/AuthService/src/main/java/com/aydindemir/manager/IUserProfileManager.java
@@ -6,10 +6,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
-@FeignClient(
-        name = "user-profile-manager",
-        url = "${services.user-profile.url}"
-)
+@FeignClient(name = "user-profile-service")
 public interface IUserProfileManager {
 
     @PostMapping("/user/save")
